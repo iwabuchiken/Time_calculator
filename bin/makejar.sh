b7248443d2241716879ebefca251505bf3448115
IwabuchiKen <iwabuchiken@ubuntu.(none)>
2013-01-19 17:27:02 +0900

    m
    1.4
    
    ---
    1. makejar.sh => Function written

--- bin/makejar.sh
+++ bin/makejar.sh
@@ -1,52 +1,89 @@
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
+#!/bin/bash
+#*****************************************
+#	Created at: 11:57:05 01/18/2013 
+#	Author: Iwabuchi Ken(iwabuchi.k.2010@gmail.com)
+#	Copyright: 
+#*****************************************
 
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
+#==============================
+#
+#	Check arguments
+#
+#==============================
+if [ $# -ne 2 ]
+then
+	echo "Usage: makejar.sh [package name] [class name]"
+	echo "	e.g. makejar.sh p1 E5.class"
+	exit 1
+fi
 
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
+#==============================
+#
+#	Func: getTrunk()
+#
+#==============================
+getTrunk() {
 
-\#$1
-\#$1
+	echo "Entering getTrunk()"
+	
+	echo "getTrunk()"
+	echo $1
 
-\#$1
-\#$1
-\#$1
+	tokens=( `echo $class_name | sed -r 's/\./ /g'` )
 
-\#$1
-\#$1
-\#$1
-\#$1
+	trunk=${tokens[0]}
 
-\#$1
-\#$1
+	echo trunk=$trunk
+	
+	echo "Exiting getTrunk()"
+}
 
-\#$1
+#==============================
+#
+#	Set variables
+#
+#==============================
+ary=( $@ )
+pkg_name=${ary[0]}
+class_name=${ary[1]}
 
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
-\#$1
+echo class_name=$class_name
+
+getTrunk $class_name
+
+#echo trunk=$trunk
+
+#==============================
+#
+#	Create manifest file,
+#	Construct a command line for generating a jar file,
+#
+#==============================
+
+
+
+#echo pkg_name=$pkg_name
+#echo class_name=$class_name
+#
+#trunk=`echo $class_name | sed -r 's/\./ /g'`
+#echo "trunk=$trunk"
+#echo "trunk[0]=$trunk[0]"
+#
+#echo -----------------------
+#for i in $trunk; do
+#	echo $i
+#done
+#
+#echo ------------2
+#trunk=( `echo $class_name | sed -r 's/\./ /g'` )
+#
+#echo ${#trunk[@]}
+#
+#echo ${trunk[0]}
+
+#==============================
+#
+#
+#
+#==============================
 
